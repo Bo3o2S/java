f84487ebc58474d3ec72689c5a6b8a1ee9ac5edd
Bo3o2S <honor2008@naver.com>
2017-05-31 22:33:06 +0900

    test update

--- 2week/Diablo3/Character/test.java
+++ 2week/Diablo3/Character/test.java
@@ -4,7 +4,7 @@ public class test{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Babarian baba = new Babarian();
+		Babarian_Skill baba = new Babarian_Skill();
 		System.out.println((int)baba.Strength);
 		baba.Level(1);
 		baba.Level(2);
